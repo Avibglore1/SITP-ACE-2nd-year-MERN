8e13a94e9d56a2dcd94d55291816c7a92ff29186
Avinash <avinashkumarofficial95@gmail.com>
2026-06-25 14:29:05 +0530

    Add .gitignore, package.json, and package-lock.json; implement basic arithmetic functions in java.js

--- landingpage/javascript/java.js
+++ landingpage/javascript/java.js
@@ -1,14 +1,22 @@
-// object is a collection of key,value pairs
+// Question: Write a function calculate(a, b, operation) that accepts two numbers and a callback function.
 
-const students = { 
-   name: "Avi",   
-   isMale: true,
-   age: 30
-};
+const x = 10;
+const y = 20;
 
-
-
-for(let obj in students){
-    console.log(students.obj);
+function add(l,m){
+  return l+m;
 }
 
+function subtract(l,m){
+  return l-m;
+}
+
+function multiply(l,m){
+  return l*m
+}
+
+function calculate(a,b,func){
+console.log(func(a,b));
+}
+
+calculate(x,y,multiply)
