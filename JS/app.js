be5058f561d8000a4ae5f8ba5d4db2170579bfc9
Avinash <avinashkumarofficial95@gmail.com>
2026-07-03 17:46:38 +0530

    Refactor app.js to remove duplicate array elements and update index.html to replace FAQ section with a simple SVG graphic

---
+++ JS/app.js
@@ -0,0 +1,14 @@
+const arr = [2,5,7,90,5];
+
+console.log(arr.indexOf(5));
+
+const arr = [1,2,2,3,4,5,5]; 
+
+
+const nonDuplicaateArray = arr.filter((item,index)=>index===arr.indexOf(item))
+
+// index: 0 to arr.length-1;
+// item: 1,2,
+
+[1,2,8,9,11]
+//x = [1,2,2,8,9,11,8]
